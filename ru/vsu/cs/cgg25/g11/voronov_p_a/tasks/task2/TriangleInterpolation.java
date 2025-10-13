be939547452a89bb2626a9e7d7035049165e9821
Pavel-V1 <pavelvoronov009@gmail.com>
2025-10-13 15:02:45 +0300

    update task2

--- ru/vsu/cs/cgg25/g11/voronov_p_a/tasks/task2/TriangleInterpolation.java
+++ ru/vsu/cs/cgg25/g11/voronov_p_a/tasks/task2/TriangleInterpolation.java
@@ -1,5 +1,32 @@
 package ru.vsu.cs.cgg25.g11.voronov_p_a.tasks.task2;
 
+import java.awt.*;
+
 public class TriangleInterpolation {
-    
+    Interpolation(Point p1, Point p2, Point p3) {
+        double x_0 = p1.x;
+        double y_0 = p1.x;
+        double x_1 = p1.x;
+        double y_1 = p1.x;
+        double x_2 = p1.x;
+        double y_2 = p1.x;
+
+
+//        sort triangle vertices by y :
+//        ( x_0 , y_0 ) // - top vertex
+//        ( x_1 , y_1 ) // - middle vertex
+//        ( x_2 , y_2 ) // - bottom vertex
+//
+//        calculate line equations
+//
+//        for ( y from y_0 to y_1 )
+//        calculate x_left , x_right with y from line equations
+//        for ( x from x_left to x_right )
+//        put_pixel (x , y) ;
+//
+//        for ( y from y_1 to y_2 )
+//        calculate x_left , x_right with y from line equations
+//        for ( x from x_left to x_right )
+//        put_pixel (x , y) ;
+    }
 }
